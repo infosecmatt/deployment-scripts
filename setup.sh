f2b231123e2b74373f7389ba7ddbe4f7aea1b8e6
Matt Johnson <me@infosecmatt.com>
2020-12-26 22:59:48 -0600

    adding to it

--- setup.sh
+++ setup.sh
@@ -2,5 +2,10 @@
 
 sudo apt update && sudo apt upgrade
 sudo apt install -y chromium-browser gnome-tweaks
+
 sudo snap install --classic code #vscode
 sudo snap install spotify
+
+cd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
