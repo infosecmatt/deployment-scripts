3f409af0084bb9b8a70f7dfcc1f481093d4ca8ee
Matt Johnson <me@infosecmatt.com>
2020-12-28 18:38:18 -0600

    added flameshot

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade
-sudo apt install -y chromium-browser vim docker docker-compose tmux
+sudo apt install -y chromium-browser vim docker docker-compose tmux flameshot
 
 sudo snap install --classic code #vscode
 sudo snap install spotify
